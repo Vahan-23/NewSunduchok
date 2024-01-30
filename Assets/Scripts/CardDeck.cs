0fb143e82b9654f9882954e165d1f836fe3eb7d0
Vahan Galstyan <91995726+Vahan-23@users.noreply.github.com>
2024-01-30 05:43:11 -0800

    init commit

---
+++ Assets/Scripts/CardDeck.cs
@@ -0,0 +1,262 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.Rendering;
+using System.Linq;
+
+public class CardDeck : MonoBehaviour
+{
+    public enum Suit { Hearts, Diamonds, Clubs, Spades };
+    public enum CardsNumber
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    };
+
+    [System.Serializable]
+    public class Card
+    {
+        public CardsNumber value; // Значение карты (2-10, валет - 11, дама - 12, король - 13, туз - 14)
+        public Suit suit; // Масть карты
+        public Sprite face; // Изображение карты лицом вверх
+        public Sprite back; // Задняя сторона карты
+        public GameObject gameObject; // Ссылка на игровой объект
+    }
+    public class CardClickHandler : MonoBehaviour
+    {
+        private Card parentCard;
+
+        public void SetParentCard(Card card)
+        {
+            parentCard = card;
+        }
+
+        public void OnMouseDown()
+        {
+            if (parentCard != null)
+            {
+                // Выводим информацию при нажатии на карту
+                Debug.Log("Нажата карта: " + parentCard.value + " " + parentCard.suit);
+            }
+        }
+    }
+
+    public List<Card> cards; // Список карт в колоде
+    public List<Card> player1Hand; // Рука игрока 1
+    public List<Card> player2Hand; // Рука игрока 2
+    public List<Card> bittenCards1;
+    public List<Card> bittenCards2;// Бита (собранные наборы)
+    [SerializeField] private List<Sprite> cardsSprites;
+    [SerializeField] private Sprite cardsSpriteBack;
+
+    // [SerializeField] private List<Transform> cardPositions;
+
+    [SerializeField] private List<Transform> player1CardPositions;
+    [SerializeField] private List<Transform> player2CardPositions;
+
+    void Start()
+    {
+        GenerateDeck();
+        ShuffleDeck();
+        DealCards();
+        // Начало игры, где игроки спрашивают друг у друга карты и проверяют на биту
+    }
+
+    void GenerateDeck()
+    {
+        cards = new List<Card>();
+
+        for (int i = 0; i <= 12; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Card newCard = new Card();
+                newCard.value = (CardsNumber)i;
+                newCard.suit = (Suit)j;
+                newCard.face = cardsSprites[i * 4 + j];
+                newCard.back = cardsSpriteBack;
+                newCard.gameObject = Instantiate(new GameObject(), Vector3.zero, Quaternion.identity); // Создаем пустой игровой объект
+                // Здесь вы можете добавить изображения для лицевой и задней стороны каждой карты
+                // Например, newCard.face = Resources.Load<Sprite>("Sprites/" + i + "_" + (Suit)j);
+                // newCard.back = Resources.Load<Sprite>("Sprites/back");
+                cards.Add(newCard);
+            }
+        }
+    }
+
+    void ShuffleDeck()
+    {
+        for (int i = 0; i < cards.Count; i++)
+        {
+            Card temp = cards[i];
+            int randomIndex = Random.Range(i, cards.Count);
+            cards[i] = cards[randomIndex];
+            cards[randomIndex] = temp;
+        }
+    }
+
+    void DealCards()
+    {
+        player1Hand = new List<Card>();
+        player2Hand = new List<Card>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            player1Hand.Add(cards[i]);
+            player2Hand.Add(cards[i + 4]);
+           
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject player1Card = Instantiate(new GameObject(), GiveCardPosition(player1Hand[i].value, player1Hand, i), Quaternion.identity);
+            SpriteRenderer renderer1 = player1Card.AddComponent<SpriteRenderer>();
+            renderer1.sprite = player1Hand[i].face;
+            renderer1.sortingOrder = 6 - GetCardOrderInLayer(player1Hand[i].value, player1Hand, i);
+            player1Card.transform.localScale = Vector3.one * 0.1f;
+            player1Card.transform.eulerAngles = new Vector3(0, 0, GetCardOrderInLayer(player1Hand[i].value, player1Hand, i) == 0 ? 0 : GetCardOrderInLayer(player1Hand[i].value, player1Hand, i) % 2 == 0 ? 5 : -5);
+            Debug.Log(player1Card.name);
+            BoxCollider2D collider = player1Card.AddComponent<BoxCollider2D>();
+
+
+            collider.size = new Vector2(12, 19);
+            collider.enabled = true;
+
+
+            //player1Hand[i].gameObject.name = "Player1Card" + i; // Устанавливаем осмысленное имя
+            player1Card.AddComponent<CardClickHandler>();
+
+            CardClickHandler clickHandler = player1Card.GetComponent<CardClickHandler>();
+            if (clickHandler == null)
+            {
+                clickHandler = player1Card.AddComponent<CardClickHandler>();
+            }
+
+            // Устанавливаем родительскую карту для CardClickHandler
+            clickHandler.SetParentCard(player1Hand[i]);
+
+
+            GameObject player2Card = Instantiate(new GameObject(), player2CardPositions[i].position, Quaternion.identity);
+            SpriteRenderer renderer2 = player2Card.AddComponent<SpriteRenderer>();
+            renderer2.sprite = player2Hand[i].back;
+            renderer2.sortingOrder = 1;
+            player2Card.transform.localScale = Vector3.one * 0.05f;
+        }
+    }
+
+
+    private Vector3 GiveCardPosition(CardsNumber number, List<Card> playerCards, int j)
+    {
+        if (j > 0)
+        {
+            // Ищем карту с заданным значением в руке игрока
+            Card requestedCard = playerCards.Find(card => card.value == number);
+
+            if (requestedCard != null && requestedCard != playerCards[j])
+            {
+                // Если карта найдена, возвращаем позицию этой карты в массиве позиций
+                Vector3 newPos = player1CardPositions[playerCards.IndexOf(requestedCard)].position;
+                List<Card> cards = new List<Card>();
+                cards = playerCards;
+                // cards.RemoveRange(j, playerCards.Count - j);
+
+                // Считаем количество карт с тем же значением, чтобы определить смещение по Y
+                int sameValueCount = cards.Count(card => card.value == number && playerCards.IndexOf(card) < j);
+
+                // Сдвигаем позицию карты по вертикали на 0.5f * sameValueCount
+                newPos = new Vector3(newPos.x, newPos.y + 0.5f * sameValueCount, 0);
+
+                return newPos;
+            }
+            // Если карта не найдена, возвращаем позицию player1CardPositions[j]
+            return player1CardPositions[j].position;
+        }
+        else
+        {
+            // Если j <= 0, возвращаем позицию player1CardPositions[j]
+            return player1CardPositions[j].position;
+        }
+    }
+
+    private int GetCardOrderInLayer(CardsNumber number, List<Card> playerCards, int j)
+    {
+
+        if (j > 0)
+        {
+            Card requestedCard = playerCards.Find(card => card.value == number);
+
+            if (requestedCard != null && requestedCard != playerCards[j])
+            {
+                return cards.Count(card => card.value == number && playerCards.IndexOf(card) < j);
+            }
+        }
+
+        return 0;
+    }
+
+
+
+    // Функция, которая обрабатывает ходы игроков
+    void PlayerMove(List<Card> currentPlayerHand, List<Card> otherPlayerHand, int requestedValue)
+    {
+        // Пример хода игрока
+        // Проверяем, есть ли у противника карта с запрошенным значением
+        for (int i = 0; i < 4; i++)
+        {
+
+
+            Card requestedCard = otherPlayerHand.Find(card => card.value == (CardsNumber)requestedValue);
+
+            if (requestedCard != null)
+            {
+                // Перемещаем карту из руки противника в руку текущего игрока
+                currentPlayerHand.Add(requestedCard);
+                otherPlayerHand.Remove(requestedCard);
+
+                // Добавляем проверку на биту (если набор собран)
+                CheckForBittenSets(currentPlayerHand, (CardsNumber)requestedValue);
+            }
+            else
+            {
+                // Обработка случая, когда противник не имеет нужной карты
+                // Можете добавить свою логику для таких ситуаций
+            }
+        }
+    }
+
+    // Проверка наличия биты (набора карт)
+    void CheckForBittenSets(List<Card> playerHand, CardsNumber number)
+    {
+        int cardsCount = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            Card requestedCard = playerHand.Find(card => card.value == number);
+
+            if (requestedCard != null)
+            {
+                cardsCount++;
+            }
+
+        }
+        if (cardsCount == 4)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Card requestedCard = playerHand.Find(card => card.value == number);
+                bittenCards1.Add(requestedCard);
+                player1Hand.Remove(requestedCard);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    void Start()
+    {
+        // Получаем ссылку на компонент кнопки
+        Button restartButton = GetComponent<Button>();
+
+        // Привязываем метод RestartScene к событию нажатия кнопки
+        restartButton.onClick.AddListener(RestartScene);
+    }
+
+    void RestartScene()
+    {
+        // Получаем индекс текущей сцены
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Перезагружаем текущую сцену
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
